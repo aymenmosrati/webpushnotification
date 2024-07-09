866f5d4bf04fe52aaf34d62e1c6103601739e6b2
aymenmosrati <aymenmosrati6@gmail.com>
2024-07-09 16:48:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import addNotification from "react-push-notification";
+import logo from "./logo.png";
 
 function App() {
+  // const clickToNotify = () => {
+  //   if (Notification.permission !== "granted") {
+  //     Notification.requestPermission().then((permission) => {
+  //       if (permission === "granted") {
+  //         showNotification();
+  //       }
+  //     });
+  //   } else {
+  //     showNotification();
+  //   }
+  // };
+
+  // const showNotification = () => {
+  //   addNotification({
+  //     title: "First Push Notification",
+  //     subtitle: "subtitle",
+  //     message: "Hello folks, this is my first notification",
+  //     // theme: "red",
+  //     // backgroundTop: "green",
+  //     // backgroundBottom: "darkgreen", //optional, background color of bottom container.
+  //     // colorTop: "green", //optional, font color of top container.
+  //     // closeButton: "Go away",
+  //     duration: 4000,
+  //     icon: logo,
+  //     native: true,
+  //     onClick: () => (window.location = "https://www.youtube.com/"),
+  //     vibrate: [100, 200, 300],
+  //     silent: false,
+  //   });
+  // };
+
+  const clickToNotify = () => {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        new Notification("Notification", {
+          title: "push Notification",
+          body: "hey how are you man",
+          // data: {
+          //   hello: "world",
+          // },
+          icon: logo,
+          onClick: (e) => window.open("https://www.youtube"),
+        });
+
+        // notification.addEventListener("click", (e) => {
+        //   e.preventDefault();
+        //   window.open("https://www.youtube.com/");
+        // });
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Push Notifications Example</h1>
+      <button onClick={clickToNotify}>Enable Push Notifications</button>
     </div>
   );
 }
